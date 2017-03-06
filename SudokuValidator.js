cc51d9a851db33baf877df831d9245ac11b00944
Lisa <misslisazhou@gmail.com>
2017-03-06 13:24:58 -1000

    validate method checks for correct numbers and duplicates in row

--- SudokuValidator.js
+++ SudokuValidator.js
@@ -16,4 +16,25 @@ function SudokuValidator(sudoku){
 
 SudokuValidator.prototype.validate = function(num){
   // Do work here
+  var checkAgainst = [];
+  for (var i = 1; i < num + 1; i++) {
+    checkAgainst.push(i);
+  }
+  for (var i = 0; i < num; i++) {
+    for (var i = 0; i < num ; i++) {
+      var truthy1 = this.sudoku[i].every(function (elem) {
+        return checkAgainst.indexOf(elem) !== -1;
+      });
+      if (truthy1 === false){
+        return false;
+      }
+      var truthy2 = this.sudoku[i].every(function (elem, idx, arr) {
+        return arr.indexOf(elem) === arr.lastIndexOf(elem);
+      })
+      if (truthy2 === false){
+        return false;
+      }
+    }
+  }
+  return true;
 }
